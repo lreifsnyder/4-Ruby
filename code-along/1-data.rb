1500549358b94f2964a0ee68ca557970b23f670d
Laura Reifsnyder <laura.reifsnyder@kellogg.northwestern.edu>
2023-04-19 23:58:45 +0000

    com

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,26 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2 + 3
 
 # Perform simple math with numbers
+puts 2/3.0
 
 # Strings
+puts "Hello, World"
 
 # Combine strings together
 
+puts "Hello," + " World"
+puts "Tacos" * 3
+
 # Variables
+food = "Tacos"
+puts food
 
 # Combine strings and variables
+
+greeting = "heeeyyyy"
+puts "#{greeting}, world"
 
 # String manipulation
